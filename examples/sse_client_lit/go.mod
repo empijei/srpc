13ee83d011b771ff3ad5205f440df19c5d8e4eb4
Roberto Clapis <empijei@gmail.com>
2026-03-10 22:24:16 +0100

    Add SSE example

---
+++ examples/sse_client_lit/go.mod
@@ -0,0 +1,7 @@
+module srpc/examples
+
+go 1.26.0
+
+require github.com/empijei/srpc v0.0.0-20260305211441-0a09f63f9711
+
+require github.com/empijei/tst v0.0.0-20260306213552-2a12dadad83d // indirect
---
+++ examples/sse_client_lit/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"io"
+	"iter"
+	"net/http"
+	"time"
+
+	"github.com/empijei/srpc"
+)
+
+type Request struct {
+	From int
+}
+
+type Response struct {
+	Value int
+}
+
+var ep = srpc.NewEndpointSeq[Response, Request](http.MethodGet, "/api/counter")
+
+//go:embed sse-client.lit.html
+var html string
+
+func main() {
+	m := http.NewServeMux()
+	m.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, html)
+	})
+	ep.Register(m, func(ctx context.Context, req Request) (iter.Seq2[Response, error], error) {
+		if req.From >= 1000 {
+			return func(yield func(Response, error) bool) {}, &srpc.WireError{
+				Msg:  "Value too high",
+				Code: http.StatusBadRequest,
+			}
+		}
+		return func(yield func(Response, error) bool) {
+			for i := req.From; i < 1000; i++ {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(1 * time.Second):
+					if !yield(Response{i}, nil) {
+						return
+					}
+				}
+			}
+		}, nil
+	})
+	addr := "localhost:8080"
+	fmt.Println("Visit http://" + addr)
+	http.ListenAndServe(addr, m)
+}
